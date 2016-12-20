b9a910877b84a02f35fc2a2dadbc196f26a87e10
Emre YILMAZ <emreyillmaz@gmail.com>
2016-12-20 21:05:50 +0300

    data send fonsiyonu eklendi.

--- Tart__projesi/Tart__projesi.ino
+++ Tart__projesi/Tart__projesi.ino
@@ -10,13 +10,23 @@ int inPin = 2; //Button giris input degeri
 int val = 0;
 float tartiVerisi = 0;
 void setup() {
-  Serial.begin(38400);
+  
+  Serial.begin(115200);
+  Serial.println("AT");
+  
   pinMode(inPin, INPUT);// inPin degerinin giris olarak tanımlandigı belirtildi.
   pinMode(9, OUTPUT); //beep sesi cikisi
-  
-  Serial.println("HX711 Demo");
-  
 
+  delay(3000); //ESP ile iletişim için 3 saniye bekliyoruz.
+  
+if(Serial.find("OK")){         //esp modülü ile bağlantıyı kurabilmişsek modül "AT" komutuna "OK" komutu ile geri dönüş yapıyor.
+     Serial.println("AT+CWMODE=1"); //esp modülümüzün WiFi modunu STA şekline getiriyoruz. Bu mod ile modülümüz başka ağlara bağlanabilecek.
+     delay(2000);
+     String baglantiKomutu=String("AT+CWJAP=\"")+ag_ismi+"\",\""+ag_sifresi+"\"";
+    Serial.println(baglantiKomutu);
+     delay(5000);
+ }
+  
   Serial.println("Initializing the scale");
   // parameter "gain" is ommited; the default value 128 is used by the library
   // HX711.DOUT	- pin #A1
@@ -62,5 +72,34 @@ void beep(unsigned char delayms){
   delay(delayms);          // wait for a delayms ms
   analogWrite(9, 0);       // 0 turns it off
   delay(delayms);          // wait for a delayms ms   
-} 
+}
+void send_data(float data){
+ Serial.println(String("AT+CIPSTART=\"TCP\",\"") + IP + "\",80");  //thingspeak sunucusuna bağlanmak için bu kodu kullanıyoruz. 
+                                                                   //AT+CIPSTART komutu ile sunucuya bağlanmak için sunucudan izin istiyoruz. 
+                                                                   //TCP burada yapacağımız bağlantı çeşidini gösteriyor. 80 ise bağlanacağımız portu gösteriyor
+ delay(1000);
+  if(Serial.find("Error")){     //sunucuya bağlanamazsak ESP modülü bize "Error" komutu ile dönüyor.
+   Serial.println("AT+CIPSTART Error");
+    return;
+  }
+  
+ String yollanacakkomut = "GET /update?key=E0P82I3TVW0KXPKD&field1=";   // Burada 64T0OS3R1OEAYUML yazan kısım bizim API Key den aldığımız Key. Siz buraya kendi keyinizi yazacaksınız.
+ yollanacakkomut += (int(data));                                      // Burada ise sıcaklığımızı float değişkenine atayarak yollanacakkomut değişkenine ekliyoruz.
+ yollanacakkomut += "\r\n\r\n";                                             // ESP modülümüz ile seri iletişim kurarken yazdığımız komutların modüle iletilebilmesi için Enter komutu yani
+  delay(3000);                                                                                // /r/n komutu kullanmamız gerekiyor.
+ 
+ Serial.print("AT+CIPSEND=");                    //veri yollayacağımız zaman bu komutu kullanıyoruz. Bu komut ile önce kaç tane karakter yollayacağımızı söylememiz gerekiyor.
+ Serial.println(yollanacakkomut.length()+2);       //yollanacakkomut değişkeninin kaç karakterden oluştuğunu .length() ile bulup yazırıyoruz.
+ 
+ delay(1000);
+ 
+ if(Serial.find(">")){                           //eğer sunucu ile iletişim sağlayıp komut uzunluğunu gönderebilmişsek ESP modülü bize ">" işareti ile geri dönüyor.
+                                                 // arduino da ">" işaretini gördüğü anda sıcaklık verisini esp modülü ile thingspeak sunucusuna yolluyor.
+ Serial.print(yollanacakkomut);
+ Serial.print("\r\n\r\n");
+ }
+ else{
+ Serial.println("AT+CIPCLOSE");
+ }
+}
 
